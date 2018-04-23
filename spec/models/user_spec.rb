0929ecdc00917e832460bf9bafeddfe6b21cb9c0
scorrea83 <correa.sergio@gmail.com>
2018-04-23 05:12:03 +0000

    Test User presence of :name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,9 @@ describe 'User' do
     expect(@user.authenticate("test")).to eq(@user)
   end
 
+  it "is invalid without a name" do
+    @user2 = User.create(:username => "Pugalicious", :email => "charlie@email.com", :password => "test")
+    expect(@user2).not_to be_valid
+  end
+
 end
