3f372ace9b4b424df9bf654a671c5c641880cb43
scorrea83 <correa.sergio@gmail.com>
2018-04-24 03:00:27 +0000

    Add List test for presence_of :title validation

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,7 +2,18 @@ require 'spec_helper'
 
 describe 'List' do
 
+  let(:attributes) do
+    {
+      title: "Watchlist",
+      description: "movies to watch"
+    }
+  end
 
+  let(:missing_title) { attributes.except(:title) }
+
+  it "is invalid without a title" do
+    expect(List.new(missing_title)).not_to be_valid
+  end
 
 
 end
