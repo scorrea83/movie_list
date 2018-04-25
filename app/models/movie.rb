30e5c4a6b22e2260bb5f0badac5936ddac0cfe2b
scorrea83 <correa.sergio@gmail.com>
2018-04-25 05:41:36 +0000

    Add has_many genres through movie_genres to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
   has_many :movie_lists
   has_many :lists, through: :movie_lists
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, presence: true, uniqueness: true
   validates_presence_of :description, :release_year
