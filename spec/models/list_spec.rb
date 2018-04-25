c86ef247474cf10e6bd96abe0e5b7924ff8d2499
scorrea83 <correa.sergio@gmail.com>
2018-04-25 02:03:22 +0000

    Test List belongs_to :user association

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -15,5 +15,13 @@ describe 'List' do
     expect(List.new(missing_title)).not_to be_valid
   end
 
+  it "belongs to a user" do
+    @user = User.create(:username => "Pugalicious", :name => "Charlie", :email => "charlie@email.com", :password => "test")
+    @list = List.new(attributes)
+
+    @user.lists << @list
+    expect(@list.user).to eq(@user)
+  end
+
 
 end
