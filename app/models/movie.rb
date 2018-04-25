d1631c5df4ccaec744e6822dd18e961fe2dc21c0
scorrea83 <correa.sergio@gmail.com>
2018-04-25 02:39:49 +0000

    Add presence_of :release_year validation to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true
-  validates_presence_of :description
+  validates_presence_of :description, :release_year
 
 end
