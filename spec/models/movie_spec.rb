fa8e989daa59d6a443fe9df487a53c2279a8a476
scorrea83 <correa.sergio@gmail.com>
2018-04-25 02:11:14 +0000

    Create movie_spec.rb for Movie model

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'Movie' do
+  let(:attributes) do
+    {
+      title: "Inception",
+      release_year: "2010",
+      description: "A thief, who steals corporate secrets through the use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO."
+    }
+  end
+  
+
+  
+  
+end
