06dd5fa2e689e66a936f4d03498e664cc82ea6c7
scorrea83 <correa.sergio@gmail.com>
2018-04-25 04:57:41 +0000

    Test has_many :movies, through: :movie_lists association for Listmodel

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -23,5 +23,18 @@ describe 'List' do
     expect(@list.user).to eq(@user)
   end
 
+  it "has many movies" do
+    @watchlist = List.create(attributes)
+    @inception = Movie.create(title: "Inception", release_year: "2010", description: "A thief, who steals corporate secrets through the use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.")
+    @interstellar = Movie.create(title: "Interstellar", release_year: "2014", description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.")
+
+
+    @watchlist.movies << @inception
+    @watchlist.movies << @interstellar
+
+    expect(@watchlist.movies).to include(@inception)
+    expect(@watchlist.movies).to include(@interstellar)
+  end
+
 
 end
