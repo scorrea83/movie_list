ed86b4d10a840a00fa5fb241483af6ade6747bdd
scorrea83 <correa.sergio@gmail.com>
2018-04-25 02:23:40 +0000

    Test Movie presence_of :title validation

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,8 +8,13 @@ describe 'Movie' do
       description: "A thief, who steals corporate secrets through the use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO."
     }
   end
-  
 
-  
-  
+  let(:missing_title) { attributes.except(:title) }
+
+  it "is invalid without a title" do
+    expect(Movie.new(missing_title)).not_to be_valid
+  end
+
+
+
 end
