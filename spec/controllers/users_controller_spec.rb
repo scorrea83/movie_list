53653db39ca543c812e7274741a790d27cc88b82
scorrea83 <correa.sergio@gmail.com>
2018-04-30 03:01:35 +0000

    List commented pseudo tests for users_controller signup page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe 'UsersController' do
 
-
+  describe 'Signing Up Page' do
+    # it loads sign up page successfully
+    # displays user show page if signup successful
+    # doesn't allow user to signup without username
+    # doesn't allow user to signup without email
+    # doesn't allow user to signup without password
+    # doesn't allow user to signup without name
+  end
 
 end
