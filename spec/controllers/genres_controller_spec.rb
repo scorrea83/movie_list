c97eb0c0b55d2d4b8d4370339972289f10c9472a
scorrea83 <correa.sergio@gmail.com>
2018-05-08 05:19:34 +0000

    Stub out genre_controller tests

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 describe 'GenresController' do
 
+  describe 'index page' do
+    context "user logged in" do
+      it "lets user view genres index page"
 
+    end
+
+    context "user not logged in" do
+      it "doesn't let user view genres index page"
+
+    end
+
+  end
 
 end
