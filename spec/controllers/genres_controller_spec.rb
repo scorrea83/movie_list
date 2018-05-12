3f5b3400fa0506b0a7a6fda81c2b7e1c559f4877
scorrea83 <correa.sergio@gmail.com>
2018-05-12 21:56:06 +0000

    Test genres index with user not logged-in

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -13,7 +13,10 @@ describe 'GenresController' do
         @genre1 = Genre.create(:title => "Action")
         @genre2 = Genre.create(:title => "Drama")
         @user1 = User.create(:name => "Charlie", :username => "Pugalicious", :email => "charlie@email.com", :password => "test")
-        post '/users/login', params
+        visit '/users/login'
+        fill_in(:username, :with => "Pugalicious")
+        fill_in(:password, :with => "test")
+        click_button 'Log In'
       end
 
       it "lets user view genres index page" do
@@ -31,7 +34,10 @@ describe 'GenresController' do
     end
 
     context "user not logged in" do
-      it "doesn't let user view genres index page"
+      it "doesn't let user view genres index page" do
+        get '/genres'
+        expect(last_response.location).to include('/users/login')
+      end
 
     end
 
