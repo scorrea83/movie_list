ddc00b7bba8d53f8135cb47d6d76b7e490e349e2
scorrea83 <correa.sergio@gmail.com>
2018-05-17 01:36:54 +0000

    Stub out movies_controller index action tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,6 +2,20 @@ require 'spec_helper'
 
 describe 'MoviesController' do
 
+  describe "index action" do
+
+    context "user logged in" do
+      it "lets user view movies index page"
+
+      it "index contains links to each movie"
+
+    end
+
+    context "user not logged in" do
+      it "doesn't let user view genres index page"
+    end
+
+  end
 
 
 end
