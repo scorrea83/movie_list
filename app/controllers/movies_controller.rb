5948e05bed4ae5a196eb2735176a85d2cd8e450d
scorrea83 <correa.sergio@gmail.com>
2018-05-22 05:46:20 +0000

    Implement movies show action
    Added get '/movies/:id' controller action to movies_controller with logged_in? verification.
    Created show_movie.erb view page to display movie info.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,13 @@ class MoviesController < ApplicationController
     end
   end
 
+  get '/movies/:id' do
+    if logged_in?
+      @movie = Movie.find(params[:id])
+      erb :'movies/show_movie'
+    else
+      redirect '/users/login'
+    end
+  end
+
 end
