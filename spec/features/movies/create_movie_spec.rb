8dc45c2eff4645cc21ef653a92e295ef4a0a5cc8
scorrea83 <correa.sergio@gmail.com>
2018-05-24 02:27:50 +0000

    Stub out Create A Movie feature spec tests

--- spec/features/movies/create_movie_spec.rb
+++ spec/features/movies/create_movie_spec.rb
@@ -10,9 +10,32 @@ describe "Create A Movie", type: :feature do
       click_button 'Log In'
     end
 
+    context "allows user to create movie" do
+      before do
+      #visit new movie page
+      #fill in form inputs movie[title], movie[release_year], movie[description]
+      #click on "Create Movie" button
+      end
+
+      it "creates a new movie"
+        #movie = Movie.find_by(name: "")
+        #expect movie to be instance of Movie
+        #Movie.all.count should be 1
+      it "redirects to new movie show page"
+        #user should be redirected to movie show page
+        #expect page.current_path to equal "/movies/#{@movie.id}"
+        #page should have movie title
+        #page should have movie description
+        #page should have movie release_year
+    end
+
+    context "given blank movie attributes" do
+      it "doesn't allow user to create new movie"
+    end
   end
 
   context "with user not logged in" do
+    it "doesn't allow user to create new movie"
   end
 
 
