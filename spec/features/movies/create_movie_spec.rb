3ece54d8db4129e4f83fa22d2a05f38aa40f6f46
scorrea83 <correa.sergio@gmail.com>
2018-05-29 02:41:47 +0000

    Add Create A Movie feature tests

--- spec/features/movies/create_movie_spec.rb
+++ spec/features/movies/create_movie_spec.rb
@@ -1,42 +1,45 @@
 require 'spec_helper'
 
 describe "Create A Movie", type: :feature do
-  context "with user logged in" do
+  before do
+    @user = User.create(:name => "Charlie", :username => "Pugalicious", :email => "charlie@email.com", :password => "test")
+    visit '/users/login'
+    fill_in(:username, :with => "Pugalicious")
+    fill_in(:password, :with => "test")
+    click_button 'Log In'
+  end
+
+  context "given all movie attributes" do
     before do
-      @user = User.create(:name => "Charlie", :username => "Pugalicious", :email => "charlie@email.com", :password => "test")
-      visit '/users/login'
-      fill_in(:username, :with => "Pugalicious")
-      fill_in(:password, :with => "test")
-      click_button 'Log In'
+      visit '/movies/new'
+      fill_in('title', with: "Die Hard")
+      fill_in('description', with: "John McClane, officer of the NYPD, tries to save his wife Holly Gennaro and several others that weretaken hostage by German terrorist Hans Gruber during a Christmas party at the Nakatomi Plaza in Los Angeles.")
+      fill_in('release_year', with: 1988)
+      click_button "Create Movie"
     end
 
-    context "allows user to create movie" do
-      before do
-      #visit new movie page
-      #fill in form inputs movie[title], movie[release_year], movie[description]
-      #click on "Create Movie" button
-      end
-
-      it "creates a new movie"
-        #movie = Movie.find_by(name: "")
-        #expect movie to be instance of Movie
-        #Movie.all.count should be 1
-      it "redirects to new movie show page"
-        #user should be redirected to movie show page
-        #expect page.current_path to equal "/movies/#{@movie.id}"
-        #page should have movie title
-        #page should have movie description
-        #page should have movie release_year
+    it "creates a new movie" do
+      movie = Movie.find_by(title: "Die Hard")
+      expect(movie).to be_an_instance_of(Movie)
+      expect(movie.title).to eq("Die Hard")
+      expect(Movie.all.count).to equal(1)
     end
 
-    context "given blank movie attributes" do
-      it "doesn't allow user to create new movie"
+    it "redirects to new movie show page" do
+      movie = Movie.find_by(title: "Die Hard")
+      expect(page.current_path).to eq("/movies/#{movie.id}")
+      expect(page.body).to include(movie.title)
+      expect(page.body).to include(movie.description)
+      expect(page.body).to include(movie.release_year)
     end
   end
 
-  context "with user not logged in" do
-    it "doesn't allow user to create new movie"
+  context "given blank movie attributes" do
+    it "doesn't allow user to create new movie" do
+      visit '/movies/new'
+      click_button "Create Movie"
+      expect(page.current_path).to eq("/movies/new")
+    end
   end
-
 
 end
