cb326eb7cc139fcf6df27d8ea8863ee92fbc5d9f
scorrea83 <correa.sergio@gmail.com>
2018-06-05 02:27:49 +0000

    Test movies-genres association in create_movie feature spec
    Add expectations to test movie-genre association during creation movie feature tests.
    Add select dropdown menu to new_movie view to pass new create_movie feature tests.

--- spec/features/movies/create_movie_spec.rb
+++ spec/features/movies/create_movie_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Create A Movie", type: :feature do
   before do
     @user = User.create(:name => "Charlie", :username => "Pugalicious", :email => "charlie@email.com", :password => "test")
+    @genre1 = Genre.create(:title => "Action")
+    @genre2 = Genre.create(:title => "Suspense")
     visit '/users/login'
     fill_in(:username, :with => "Pugalicious")
     fill_in(:password, :with => "test")
@@ -15,6 +17,9 @@ describe "Create A Movie", type: :feature do
       fill_in('title', with: "Die Hard")
       fill_in('description', with: "John McClane, officer of the NYPD, tries to save his wife Holly Gennaro and several others that weretaken hostage by German terrorist Hans Gruber during a Christmas party at the Nakatomi Plaza in Los Angeles.")
       fill_in('release_year', with: 1988)
+      select('Action', from: 'movie[genre_ids][]')
+      select('Suspense', from: 'movie[genre_ids][]')
+
       click_button "Create Movie"
     end
 
@@ -23,6 +28,7 @@ describe "Create A Movie", type: :feature do
       expect(movie).to be_an_instance_of(Movie)
       expect(movie.title).to eq("Die Hard")
       expect(Movie.all.count).to equal(1)
+      expect(movie.genre_ids).to include(@genre1.id, @genre2.id)
     end
 
     it "redirects to new movie show page" do
@@ -30,7 +36,8 @@ describe "Create A Movie", type: :feature do
       expect(page.current_path).to eq("/movies/#{movie.id}")
       expect(page.body).to include(movie.title)
       expect(page.body).to include(movie.description)
-      expect(page.body).to include(movie.release_year)
+      expect(page.body).to include("#{movie.release_year}")
+      expect(page.body).to include(@genre1.title, @genre2.title)
     end
   end
 
@@ -38,7 +45,7 @@ describe "Create A Movie", type: :feature do
     it "doesn't allow user to create new movie" do
       visit '/movies/new'
       click_button "Create Movie"
-      expect(page.current_path).to eq("/movies/new")
+      expect(page.body).to include("Add A New Movie")
     end
   end
 
