45eb93b6ae32ea71c99ebb1459dee6f428313326
scorrea83 <correa.sergio@gmail.com>
2018-06-15 01:51:46 +0000

    Add edit_movie given success feature tests

--- spec/features/movies/edit_movie_spec.rb
+++ spec/features/movies/edit_movie_spec.rb
@@ -8,23 +8,37 @@ describe "Edit A Movie", type: :feature do
     @movie = Movie.create(:title => "Die Hard", :description => "John McClane, officer of the NYPD, tries to save his wife Holly Gennaro and several others that were taken hostage by German terrorist Hans Gruber during a Christmas party at the Nakatomi Plaza in Los Angeles.", :release_year => 1988)
     @movie.genres << @genre1
     @movie.genres << @genre2
-    binding.pry
     visit '/users/login'
     fill_in(:username, :with => "Pugalicious")
     fill_in(:password, :with => "test")
     click_button 'Log In'
-
     end
+
     context "given success" do
       before do
-        #visit "/movies/#{@movie.id}/edit"
-        #fill_in('title', with: "Die Hard, A Christmas Story")
-        #click_button "Save Changes"
+        @genre3 = Genre.create(:title => "Comedy")
+        visit "/movies/#{@movie.id}/edit"
       end
 
-      it "edits a movie"
-      it "redirects to movie show page after edit"
+      it "changes a movie title" do
+        fill_in('title', with: "Die Hard, A Christmas Story")
+        click_button "Save Changes"
+        movie = Movie.find(@movie.id)
 
+        expect(movie.title).to eq("Die Hard, A Christmas Story")
+        expect(page.current_path).to eq("/movies/#{movie.id}")
+      end
+
+      it "changes a movie's genres" do
+        unselect('Suspense', from: 'movie[genre_ids][]')
+        select('Comedy', from: 'movie[genre_ids][]')
+        click_button "Save Changes"
+        movie = Movie.find(@movie.id)
+
+        expect(page.current_path).to eq("/movies/#{movie.id}")
+        expect(movie.genre_ids).to include(@genre1.id, @genre3.id)
+        expect(movie.genre_ids).to_not include(@genre2.id)
+      end
     end
 
     context "given failure" do
